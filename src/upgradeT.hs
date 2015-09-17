2c3142b481c3a53ae8fbad008cd2294daea717b0
Adolfo Pérez Álvarez <adolfo.pa@gmail.com>
2015-09-17 23:23:17 +0200

    Parameterize connection type

--- src/upgradeT.hs
+++ src/upgradeT.hs
@@ -8,39 +8,39 @@ import Database.HDBC
 import Database.HDBC.Sqlite3
 import Text.Printf
 
-type DB m = ReaderT Connection m ()
+type DB c m = ReaderT c m ()
 
-newtype DBCmd m a = DBCmd { getDB :: ReaderT Connection m a  }
+newtype DBCmd c m a = DBCmd { getDB :: ReaderT c m a  }
 
-instance Monad t => Monad (DBCmd t) where
+instance Monad m' => Monad (DBCmd c m') where
     m >>= f = DBCmd $ getDB m >>= getDB . f
     return  = DBCmd . return
 
-sql :: String -> DB IO
+sql :: String -> DB Connection IO
 sql q = do c <- ask
            liftIO $ run c q []
            return ()
 
-query :: ([[SqlValue]] -> a) -> String -> DBCmd IO a
+query :: ([[SqlValue]] -> a) -> String -> DBCmd Connection IO a
 query f q =
     DBCmd $ do c <- ask
                r <- liftIO $ quickQuery c q []
                return $ f r
 
-whenC :: DBCmd IO Bool -> DBCmd IO () -> DB IO
+whenC :: DBCmd Connection IO Bool -> DBCmd Connection IO () -> DB Connection IO
 whenC p cmd = do b <- getDB p
                  when b $ getDB cmd
 
-tableExists :: String -> DBCmd IO Bool
+tableExists :: String -> DBCmd Connection IO Bool
 tableExists tab =
     query (\xs -> length xs == 1) $
               printf "SELECT name FROM sqlite_master WHERE type = 'table' AND name = '%s'" tab
 
-addColumn :: String -> String -> String -> DBCmd IO ()
+addColumn :: String -> String -> String -> DBCmd Connection IO ()
 addColumn tab col typ =
     DBCmd $ sql $ printf "ALTER TABLE %s ADD COLUMN %s %s" tab col typ
 
-upgrade :: DB IO
+upgrade :: DB Connection IO
 upgrade = do
   sql "CREATE TABLE DOC (ID PRIMARY KEY, NAME VARCHAR(75) NOT NULL)"
   whenC (tableExists "DOC") $ do
