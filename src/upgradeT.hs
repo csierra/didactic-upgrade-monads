15250ba55e3642927f61b7207346d71aeb2d361b
Adolfo Pérez Álvarez <adolfo.pa@gmail.com>
2015-09-18 10:26:07 +0200

    Generalize type signatures a little more

--- src/upgradeT.hs
+++ src/upgradeT.hs
@@ -10,7 +10,7 @@ import Text.Printf
 
 data Ctx c b = Ctx { getConn :: c
                    , runDDL :: c -> String -> IO ()
-                   , runDQL :: c -> String -> IO b }
+                   , runDQL :: c -> String -> IO [b] }
 
 type DB c b m = ReaderT (Ctx c b) m ()
 
@@ -20,30 +20,30 @@ instance Monad m' => Monad (DBCmd c b m') where
     m >>= f = DBCmd $ getDB m >>= getDB . f
     return  = DBCmd . return
 
-sql :: String -> DB Connection [[SqlValue]] IO
+sql :: String -> DB c b IO
 sql q = do c <- ask
            liftIO $ (runDDL c) (getConn c) q
 
-query :: ([[SqlValue]] -> a) -> String -> DBCmd Connection [[SqlValue]] IO a
+query :: ([b] -> a) -> String -> DBCmd c b IO a
 query f q =
     DBCmd $ do c <- ask
                r <- liftIO $ (runDQL c) (getConn c) q
                return $ f r
 
-whenC :: DBCmd Connection [[SqlValue]] IO Bool -> DBCmd Connection [[SqlValue]] IO () -> DB Connection [[SqlValue]] IO
+whenC :: DBCmd c b IO Bool -> DBCmd c b IO () -> DB c b IO
 whenC p cmd = do b <- getDB p
                  when b $ getDB cmd
 
-tableExists :: String -> DBCmd Connection [[SqlValue]] IO Bool
+tableExists :: String -> DBCmd c b IO Bool
 tableExists tab =
     query (\xs -> length xs == 1) $
               printf "SELECT name FROM sqlite_master WHERE type = 'table' AND name = '%s'" tab
 
-addColumn :: String -> String -> String -> DBCmd Connection [[SqlValue]] IO ()
+addColumn :: String -> String -> String -> DBCmd c b IO ()
 addColumn tab col typ =
     DBCmd $ sql $ printf "ALTER TABLE %s ADD COLUMN %s %s" tab col typ
 
-upgrade :: DB Connection [[SqlValue]] IO
+upgrade :: DB c b IO
 upgrade = do
   sql "CREATE TABLE DOC (ID PRIMARY KEY, NAME VARCHAR(75) NOT NULL)"
   whenC (tableExists "DOC") $ do
